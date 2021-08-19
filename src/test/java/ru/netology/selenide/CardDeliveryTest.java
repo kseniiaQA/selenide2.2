d2fd41e764155bf081c6047999b552efe1240500
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-08-19 11:25:40 +0300

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -16,34 +16,26 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
-
-
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999");
     }
 
-    @Test
-    public void shouldCheckRequest() {
+ @Test
+    public void shouldSubmitRequest() {
         SelenideElement form = $("[method=post]");
-       form.$("[data-test-id=city] input").setValue("Саратов");
+        form.$("[data-test-id=city] input").setValue("Абакан");
         form.$("[data-test-id=date] input").doubleClick();
-        form.$("[data-test-id=date] input").sendKeys("Keys.DELETE");
+        form.$("[data-test-id=date] input").sendKeys(Keys.DELETE);
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_MONTH, 3);
         form.$("[data-test-id=date] input").setValue(new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime()));
-        form.$("[data-test-id=name] input").setValue("Мария Петрова");
-        form.$("[data-test-id=phone] input").setValue("+79769548312");
+        form.$("[data-test-id=name] input").setValue("Анна");
+        form.$("[data-test-id=phone] input").setValue("+71234123890");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button] .button__content").click();
         $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime())));
-
-
-
-
-
     }
-
 
 
 }
